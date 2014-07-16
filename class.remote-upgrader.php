f53ddcdccbed42ffed151682023233bb47b3da39
George Stephanis <georgestephanis@automattic.com>
2014-07-16 14:38:47 -0400

    Slug out our main seven public functions.

--- class.remote-upgrader.php
+++ class.remote-upgrader.php
@@ -1,5 +1,54 @@
 <?php
 
 class Jetpack_Remote_Upgrader {
-	
+
+	/**
+	 * Upgrade Core
+	 */
+	public function upgrade_core( $args = array() ) {
+		return true;
+	}
+
+	/**
+	 * Install a Plugin
+	 */
+	public function install_plugin( $args = array() ) {
+		return true;
+	}
+
+	/**
+	 * Upgrade a Plugin
+	 */
+	public function upgrade_plugin( $args = array() ) {
+		return true;
+	}
+
+	/**
+	 * Uninstall a Plugin
+	 */
+	public function uninstall_plugin( $args = array() ) {
+		return true;
+	}
+
+	/**
+	 * Install a Theme
+	 */
+	public function install_theme( $args = array() ) {
+		return true;
+	}
+
+	/**
+	 * Upgrade a Theme
+	 */
+	public function upgrade_theme( $args = array() ) {
+		return true;
+	}
+
+	/**
+	 * Uninstall a Theme
+	 */
+	public function uninstall_theme( $args = array() ) {
+		return true;
+	}
+
 }
